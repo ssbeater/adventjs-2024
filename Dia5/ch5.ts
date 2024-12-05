cbee5fa27548fa91d544df6c4a1274b4a8eb158f
Gridman <santiagosuarez.beater@outlook.com>
2024-12-05 04:55:37 -0500

    Improve Solution Day 5

--- Dia5/ch5.ts
+++ Dia5/ch5.ts
@@ -7,19 +7,15 @@ export function organizeShoes(shoes: Shoe[]): number[] {
     const sizeMap: Record<number, { left: number; right: number }> = {};
     const available: number[] = [];
 
-    shoes.forEach((shoe) => {
-        const { size, type } = shoe;
-
-        if (!sizeMap[size]) {
-            sizeMap[size] = { left: 0, right: 0 };
+    for (const shoe of shoes) {
+        if (!sizeMap[shoe.size]) {
+            sizeMap[shoe.size] = { left: 0, right: 0 };
         }
 
-        if (type === "I") {
-            sizeMap[size].left++;
-        } else {
-            sizeMap[size].right++;
-        }
-    });
+        shoe.type === "I"
+            ? sizeMap[shoe.size].left++
+            : sizeMap[shoe.size].right++;
+    }
 
     for (const size in sizeMap) {
         const { left, right } = sizeMap[size];
